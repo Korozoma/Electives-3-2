1dd222e3d0d2422cc6aa77ba6e213e2b2ea38bea
Korozoma <104410029+Korozoma@users.noreply.github.com>
2022-07-29 10:12:52 +0800

    Delete Exercise1.m

--- Exercise1.m
+++
@@ -1,7 +0,0 @@
-f = imread ('C:\Users\IT-LITERACY_TRAINING\Documents\sunflower.jpg');
-imwrite (f, 'C:\Users\IT-LITERACY_TRAINING\Documents\sunflower2.png');
-g = imshow (f);
-k = rgb2gray(f);
-imwrite (k, 'C:\Users\IT-LITERACY_TRAINING\Documents\sunflower3.png');
-imshow (k);
-whos k;
